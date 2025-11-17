817c2a3a123d911e8ed719aa7c819e758a6829e3
Francisca <anafranciscapacheco28@gmail.com>
2025-11-17 18:04:43 +0000

    lab 3

--- PFL/ex03.hs
+++ PFL/ex03.hs
@@ -14,68 +14,65 @@ replicate_ :: Int -> a -> [a]
 replicate_ n x  | n<=0 = []
                 | otherwise = x:replicate_ (n-1) x
 
-myIndex_ :: [a] -> Int -> a
-myIndex_ x 0 = head x
-myIndex_ x n = myIndex_ (drop 1 x) (n-1)
+index :: [a] -> Int -> a
+index x 0 = head x
+index x n = index (drop 1 x) (n-1)
 
 elem_ :: Eq a => a -> [a] -> Bool
-elem_ a [] = False
-elem_ a (x:xs)  |x == a = True
-                | otherwise = elem_ a xs
+elem_ n [] = False
+elem_ n (x:xs)
+    | n == x = True
+    | otherwise = elem_ n xs
 
 leastDiv :: Integer -> Integer
-leastDiv a  = aux a 2
-    where
-    aux :: Integer -> Integer -> Integer
-    aux n d
-        | d*d > n = n
-        | mod n d == 0 = d
-        | otherwise = aux n (d+1)
+leastDiv n = let res= [x | x<- [2..(floor(sqrt(fromIntegral n) ))], mod n x == 0]
+             in if null res then n else head res
 
 isPrimeFast :: Integer -> Bool
-isPrimeFast n | n <= 1 = False
-            | leastDiv n == n = True
-            | otherwise = False
+isPrimeFast n
+    | n<=1 = False
+    | leastDiv n == n = True
+    | otherwise = False
 
 nub :: Eq a => [a] -> [a]
 nub [] = []
-nub (x:xs) = x : nub (filter (\y -> y /= x) xs)
+nub (x:xs) = x: nub (removeEx x xs)
 
-intersperse_ :: a -> [a] -> [a]
-intersperse_ n [] = []
-intersperse_ n [a] = [a]
-intersperse_ n (x:xs) = x : n : intersperse_ n xs
+removeEx :: Eq a => a -> [a] -> [a]
+removeEx n [] = []
+removeEx n (y:ys)
+    | n == y = removeEx n ys
+    | otherwise = y: removeEx n ys
 
-insert :: Ord a => a-> [a] -> [a]
+intersperse :: a -> [a] -> [a]
+intersperse n [] = []
+intersperse n [a] = [a]
+intersperse n (x:xs) = x:n: intersperse n xs
+
+insert :: Ord a => a -> [a] -> [a]
 insert n [] = [n]
-insert n (x:xs) | n <= x = n : x : xs
-                | otherwise = x : insert n xs
+insert n (x:xs)
+    | n <= x = (n : x : xs)
+    | otherwise = x : insert n xs
 
 isort :: Ord a => [a] -> [a]
 isort [] = []
+isort [a] = [a]
 isort (x:xs) = insert x (isort xs)
 
-merge :: Ord a =>[a] ->[a] -> [a]
-merge [] y = y
-merge x [] = x
-merge (x:xs) (y:ys) |x<= y = x:merge xs (y:ys)
-                    |y< x = y:merge (x:xs) ys
-
-msort :: Ord a =>[a] ->[a]
-msort [] = []
-msort [a] = [a]
-msort xs = merge (msort left) (msort right)
-    where
-    mid= div (length xs) 2
-    left = take mid xs
-    right = drop mid xs
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] a = a
+merge a [] = a
+merge (x:xs) (y:ys)
+    | x <= y = x : merge xs (y:ys)
+    | y <= x = y : merge (x:xs) ys
 
 toBits :: Int -> [Int]
-toBits n = reverse (toBitsaux n)
+toBits n = reverse (aux n)
 
-toBitsaux :: Int -> [Int]
-toBitsaux 0 = []
-toBitsaux x = (mod x 2) : toBitsaux (div x 2)
+aux :: Int ->[ Int]
+aux 0 = []
+aux n = [mod n 2] ++ aux (div n 2)
 
 fromBits :: [Int] -> Int
 fromBits x = fromBitsAux 0 x
@@ -85,7 +82,37 @@ fromBitsAux n [] = n
 fromBitsAux n (x:xs) = fromBitsAux(n * 2 + x) xs
 
 divisors :: Integer -> [Integer]
-divisors n = filter (\x -> mod n x == 0)[1..n]
+divisors n = filter (\x -> mod n x == 0 ) [1..n]
 
 isPrimeFast_ :: Integer -> Bool
-isPrimeFast_
+isPrimeFast_ n = not (any (\x -> mod n x == 0) [2..floor(sqrt (fromIntegral n))])
+
+myappend :: [a] -> [a] -> [a]
+myappend xs ys = foldr (:) ys xs
+
+myconcat :: [[a]] -> [a]
+myconcat ys = foldr (++) [] ys
+
+myreverse :: [a] -> [a]
+myreverse xs = foldl (\y x -> x : y) [] xs
+
+myelem ::  Eq a => a -> [a] -> Bool
+myelem n xs = foldr (\x acc-> if (n == x) || acc then True else False) False xs
+
+sndfromBits :: [Int] -> Int
+sndfromBits xs = foldl (\acc x -> acc * 2 + x) 0 xs
+
+group :: Eq a => [a] -> [[a]]
+group [] = []
+group (x:xs) =
+    let ys = takeWhile (==x) xs
+        zs = dropWhile (==x) xs
+    in (x:ys) : group zs
+
+intercalate :: a -> [a] -> [[a]]
+intercalate n [] = [[n]]
+intercalate n (x:xs) = (n : x : xs) : [x : zs | zs <- intercalate n xs]
+
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations (x:xs) = concat [ intercalate x p | p <- permutations xs ]
