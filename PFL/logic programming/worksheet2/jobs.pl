0b0e67cda2a560f0ce112e2b6906fc832774c35e
Francisca <anafranciscapacheco28@gmail.com>
2026-01-09 18:19:27 +0000

    jobs

---
+++ PFL/logic programming/worksheet2/jobs.pl
@@ -0,0 +1,26 @@
+job(technician, eleuterio).
+job(technician, juvenaldo).
+job(analyst, leonilde).
+job(analyst, marciliano).
+job(engineer, osvaldo).
+job(engineer, porfirio).
+job(engineer, reginaldo).
+job(supervisor, sisnando).
+job(chief_supervisor, gertrudes).
+job(secretary, felismina).
+job(director, asdrubal).
+
+supervised_by(technician, engineer).
+supervised_by(engineer, supervisor).
+supervised_by(analyst, supervisor).
+supervised_by(supervisor, chief_supervisor).
+supervised_by(chief_supervisor, director).
+supervised_by(secretary, director).
+
+superior(X, Y) :-
+    job(JX, X),
+    job(JY, Y),
+    (
+        supervised_by(JY, JX);
+        supervised_by(JY, Z), superior(Z, JX)
+    ).
